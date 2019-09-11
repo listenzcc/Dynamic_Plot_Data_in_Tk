6a971abc3c7f27745a60af69678b73494c4e7175
zcc <listenzcc@mail.bnu.edu.cn>
2019-09-11 14:13:47 +0800

    daily sync

--- tkapp_dynamic_plot.py
+++ tkapp_dynamic_plot.py
@@ -16,12 +16,15 @@ from local_toolbox import Buffer
 
 class App():
     def __init__(self, root, frame_rate=20):
+        # Initialize root.
         self.root = root
+
         # Initialize frames.
         self.frames = dict(
             Figure=tk.Frame(root),  # Frame of figure.
             Control=tk.Frame(root),  # Frame of controls.
         )
+
         # Initialize buttons.
         self.buttons = dict(
             # Go button, start waveforms.
@@ -33,9 +36,9 @@ class App():
         self.display_info = dict(
             frame_rate=frame_rate,  # Frame rate of animation.
             display_on=False,  # Display switcher.
-            idx=0,  # Current horizontal index.
+            idx=0,  # Current vertical index.
             max_length=100,  # Max_length of display.
-            channels=[5, 7, 9],  # Channels to display.
+            channels=range(13),  # Channels to display.
             height=2,  # Height of each channel.
         )
 
@@ -85,7 +88,6 @@ class App():
         canvas.mpl_connect('key_press_event', self.on_key_event)
 
         # Bound _start_thread function on Go button.
-        # self.buttons['Go'].config(command=self._run)
         self.buttons['Display'].config(command=self._realtime_display)
 
         self.buffer = buffer
@@ -94,8 +96,10 @@ class App():
         self.axe = axe
 
     def _biased(self, data, j):
-        # Compute bias for j-th channels
-        return data[:, j] + j * self.display_info['height']
+        # Compute bias for j-th channel.
+        bias = j * self.display_info['height']
+        # Return biased data of j-th channel.
+        return data[:, j] + bias
 
     def _start_thread(self):
         # Initialize and start thread for _run.
@@ -104,32 +108,67 @@ class App():
 
     def _realtime_display(self):
         if self.display_info['display_on']:
+            # Change button text into ON
             self.buttons['Display'].config(text='Display_ON')
+            # Turn off display
             self.display_info['display_on'] = False
         else:
+            # Change button text into OFF
             self.buttons['Display'].config(text='Display_OFF')
+            # Turn on display
             self.display_info['display_on'] = True
 
+        # Compute lag based on frame_rate.
+        # lag is the time lag between frames.
         lag = 1 / self.display_info['frame_rate']
 
+        # Begin dynamic display loop if display is on.
         while self.display_info['display_on']:
+            # Stamp the frame begin.
             begin = time.time()
-            self.buffer.push(np.random.randn(1, self.buffer.info['channels']))
 
-            self.axe.redraw_in_frame()
+            # Update buffer.
+            # TODO: The update buffer should be perform in a seperate thread.
+            self.buffer.push(np.random.randn(np.random.randint(1, 5),
+                                             self.buffer.info['channels']))
+
+            # Read new_data from buffer.
             new_data = self.buffer.pop()
-            for j, line in self.lines.items():
-                data = line[0].get_ydata()
-                data[self.display_info['idx']] = self._biased(new_data, j)
-                line[0].set_ydata(data)
-            self.display_info['idx'] += 1
-            self.display_info['idx'] %= self.display_info['max_length']
+            if new_data is None:
+                # Continue loop if no new_data.
+                continue
 
+            # Redraw background frame.
+            self.axe.redraw_in_frame()
+
+            for j, line in self.lines.items():
+                # Update y_data of j-th line.
+                # Get idx and data of the line.
+                line_idx = self.display_info['idx']
+                line_data = line[0].get_ydata()
+                # Update line_data for each value in new_data.
+                for x in self._biased(new_data, j):
+                    line_data[line_idx] = x
+                    # Update line_idx
+                    line_idx += 1
+                    line_idx %= self.display_info['max_length']
+                # Write ydata of the line.
+                line[0].set_ydata(line_data)
+
+            # Updata vertical idx.
+            self.display_info['idx'] = line_idx
+
+            # Blit canvas.
             self.fig.canvas.blit(self.axe.bbox)
+            # Flush canvas.
             self.fig.canvas.flush_events()
 
             if lag > (time.time() - begin):
+                # Wait for remaining lagging time.
                 time.sleep(lag - (time.time() - begin))
+            else:
+                # Report lagging, if a frame can not be updated on time.
+                print('Lagging.')
 
     def on_key_event(self, event):
         # Handel key press.
